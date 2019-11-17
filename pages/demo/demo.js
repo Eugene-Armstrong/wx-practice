c275c089059cb880c9e703f3984c1bc3c8f4febd
lujinfeng <1020454354@qq.com>
2019-11-17 17:19:34 +0800

    init wx-ui demo project

---
+++ pages/demo/demo.js
@@ -0,0 +1,30 @@
+// pages/demo/demo.js
+Page({
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
+    }
+  },
+
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
+  },
+
+  changeAutoplay() {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  }
+})
